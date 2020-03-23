bded4a44b66b8dd206e36f07372d5a387be2d316
Jennina Farm <jennina.farm94@gmail.com>
2020-03-23 14:28:05 +0200

    commit

--- core/src/com/mygdx/game/MapScreen.java
+++ core/src/com/mygdx/game/MapScreen.java
@@ -245,7 +245,7 @@ public class MapScreen extends ApplicationAdapter implements Screen, GestureList
 
     @Override
     public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-
+        System.out.println("pinching");
         return false;
     }
 
