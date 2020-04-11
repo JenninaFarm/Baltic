4795a64e218036a3fca5e65e6e0b1bf69f583576
Jennina Farm <jennina.farm94@gmail.com>
2020-04-11 16:25:32 +0300

    farms Locked and set bought boolean

--- core/src/com/mygdx/game/MapButton.java
+++ core/src/com/mygdx/game/MapButton.java
@@ -3,25 +3,33 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 
+import java.awt.TextField;
 import java.time.ZonedDateTime;
 
 public class MapButton extends Actor {
 
     private Main main;
     private Texture button;
+    private Texture notBought;
+    private boolean bought = false;
     private float width;
     private float height;
     private int index;
+    private int cost;
 
     public MapButton(Main m, int x, int y, int i) {
 
         index = i;
         main = m;
+        cost = (int)(1000* Math.pow(10, (i-1)));
+        System.out.println(cost);
         button = new Texture(Gdx.files.internal("farm-icon.png"));
+        notBought = new Texture(Gdx.files.internal("test_icon.jpg"));
         setX(x);
         setY(y);
         width = button.getWidth()/7f;
@@ -32,15 +40,29 @@ public class MapButton extends Actor {
 
         addListener(new InputListener() {
             public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                System.out.println("to farm");
-                main.switchScreen(3, index);
+                if(bought) {
+                    System.out.println("to farm");
+                    main.switchScreen(3, index);
+                } else {
+                    int currentMoney = main.nonStaticGetMoney();
+                    if(currentMoney >= cost) {
+                        bought = true;
+                        main.setMoney(currentMoney-cost);
+                    }
+                }
                 return true;
             }
         });
     }
 
     public void draw(Batch batch, float alpha) {
-        batch.draw(button, this.getX(), this.getY(), width, height);
+        if(!bought && index != 0) {
+            batch.draw(notBought, this.getX(), this.getY(), width, height);
+        } else {
+            batch.draw(button, this.getX(), this.getY(), width, height);
+            //sets the first farm bought
+            bought = true;
+        }
     }
 
     @Override
