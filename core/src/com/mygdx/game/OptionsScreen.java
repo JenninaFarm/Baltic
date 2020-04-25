83aa0ad75727d799b8ae65ef578283e709f3b079
Jennina Farm <jennina.farm94@gmail.com>
2020-04-25 19:42:08 +0300

    OptionsScreen JavaDoc

--- core/src/com/mygdx/game/OptionsScreen.java
+++ core/src/com/mygdx/game/OptionsScreen.java
@@ -7,25 +7,62 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.I18NBundle;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 import java.util.ArrayList;
 
+/**
+ * OptionsScreen is an object base class to create a Screen for the options.
+ *
+ * @author  Jennina Färm
+ * @author  Tommi Häkkinen
+ * @version 2020.2204
+ * @since 1.8
+ */
 public class OptionsScreen implements Screen {
 
+    /**
+     * Main to ask I18NBundle and pass over to Actors
+     */
     private Main main;
+
+    /**
+     * Handles drawing
+     */
     private SpriteBatch batch;
+
+    /**
+     * Stage to place Actors that are not moving with the camera
+     */
     private Stage stage;
+
+    /**
+     * Actor that creates background for the MapScreen
+     */
     private Background background;
 
+    /**
+     * ArrayList containing OptionsButton Actors
+     */
     private ArrayList<OptionsButton> optionsButtons = new ArrayList<>();
+
+    /**
+     * Amount of optionsButtons
+     */
     private int buttonAmount = 5;
+
+    /**
+     * Actor of the ReturnButton to get back to previous screen
+     */
     private ReturnButton returnButton;
 
-
-    public OptionsScreen(Main m) {
+    /**
+     * Constructor. Creates all of the private variables, arrays and objects and adds Actors to the stage.
+     *
+     * @param m Main contains meta data of the game
+     */
+    OptionsScreen(Main m) {
         main = m;
         batch = main.getBatch();
         stage = new Stage(new FitViewport(800, 450), batch);
@@ -39,6 +76,9 @@ public class OptionsScreen implements Screen {
         Gdx.input.setInputProcessor(stage);
     }
 
+    /**
+     * Creates OptionsButtons and ReturnButton
+     */
     private void createButtons() {
         I18NBundle myBundle = main.getMyBundle();
         for(int i=0; i<buttonAmount; i++) {
@@ -47,6 +87,9 @@ public class OptionsScreen implements Screen {
         returnButton = new ReturnButton(main, 1);
     }
 
+    /**
+     * Adds actors to the stage
+     */
     private void addActors() {
         stage.addActor(background);
         for(int i=0; i<buttonAmount; i++) {
@@ -61,18 +104,26 @@ public class OptionsScreen implements Screen {
 
     }
 
+    /**
+     * Calls draw- and act -methods of the Stage.
+     *
+     * @param delta delta time of player's device
+     */
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.end();
 
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
     }
 
-    public Stage getStage() {
+    /**
+     * Get -method to collect stage
+     *
+     * @return stage of the MapScreen
+     */
+    Stage getStage() {
         return stage;
     }
 
@@ -96,6 +147,9 @@ public class OptionsScreen implements Screen {
 
     }
 
+    /**
+     * Disposes stage
+     */
     @Override
     public void dispose() {
         stage.dispose();
